9455fd22c4cdcfbb01240cf15b1ceee9cdbb21e9
Raddai249 <raddaikabongo7@gmail.com>
2026-01-01 22:35:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-li.remove();
-saveTasks();
-updateCounter();
+function filterTasks(type) {
+  const tasks = document.querySelectorAll("#taskList li");
+
+  tasks.forEach(task => {
+    if (type === "all") task.style.display = "flex";
+    if (type === "active")
+      task.style.display = task.classList.contains("done") ? "none" : "flex";
+    if (type === "done")
+      task.style.display = task.classList.contains("done") ? "flex" : "none";
+  });
+}
